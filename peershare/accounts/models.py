de64bb1489454b4c7d9aac64c543ea5b602b6a6b
ariha Tasnim <farihatasnim218@gmail.com>
2026-05-07 23:19:48 +0600

    init

--- peershare/accounts/models.py
+++ peershare/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    university_email = models.EmailField(
+        unique=True, blank=True, default=''
+    )
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
