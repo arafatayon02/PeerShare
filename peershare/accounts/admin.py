4cb0ce8c1b86624cafdd7dacc02b810bc100a705
ariha Tasnim <farihatasnim218@gmail.com>
2026-05-07 23:14:26 +0600

    init

---
+++ peershare/accounts/admin.py
@@ -0,0 +1,16 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display  = ['username', 'university_email', 'is_verified', 'date_joined']
+    list_filter   = ['is_verified']
+    search_fields = ['username', 'university_email']
+    fieldsets     = UserAdmin.fieldsets + (
+        ('University Info', {'fields': ('university_email', 'is_verified')}),
+    )
