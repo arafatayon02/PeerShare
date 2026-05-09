d724cc4d23b04d5a0bd7f98daecd808fcbe43d87
ariha Tasnim <farihatasnim218@gmail.com>
2026-05-09 22:10:37 +0600

    wh589

--- peershare/accounts/models.py
+++ peershare/accounts/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
 class CustomUser(AbstractUser):
+
     university_email = models.EmailField(
         unique=True,
         blank=True,
