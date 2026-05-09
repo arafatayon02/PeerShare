753af15b012a2c3197f886336f5a8e398d51e25c
ariha Tasnim <farihatasnim218@gmail.com>
2026-05-09 22:04:16 +0600

    whhh

--- peershare/accounts/admin.py
+++ peershare/accounts/admin.py
@@ -1,9 +1,3 @@
-
-# Register your models here.
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display  = ['username', 'university_email', 'is_verified', 'date_joined']
