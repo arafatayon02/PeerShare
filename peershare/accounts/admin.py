6b531920119fd3231f4d29a3e005cf062b8c459d
arafatayon02 <arafatrahman133a@gmail.com>
2026-05-13 22:10:18 +0600

    fixing code

--- peershare/accounts/admin.py
+++ peershare/accounts/admin.py
@@ -1,3 +1,7 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display  = ['username', 'university_email', 'is_verified', 'date_joined']
