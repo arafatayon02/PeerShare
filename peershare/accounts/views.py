4246fe1e96c9f01f1b14760a8116e28a8fb0ff3f
ariha Tasnim <farihatasnim218@gmail.com>
2026-05-15 01:40:32 +0600

    kp

--- peershare/accounts/views.py
+++ peershare/accounts/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 def home(request):
+
     users = User.objects.all()
+
+
     return render(
         request,
         'home.html',
